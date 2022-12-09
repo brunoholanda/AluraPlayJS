f69db44e1ce4cc2fcb5141e004cabbd56785650c
Bruno Holanda <holanda_rodrigues@hotmail.com>
2022-12-09 18:33:32 -0300

    Add files via upload

---
+++ js/conectaApi.js
@@ -0,0 +1,41 @@
+async function listaVideos() {
+    const conexao = await fetch("http://localhost:3000/videos");
+    const conexaoConvertida = await conexao.json();
+   
+    return conexaoConvertida;
+}
+
+async function criaVideo(titulo, descricao, url, imagem) {
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil vizualizacoes`,
+            url: url,
+            imagem: imagem
+        })
+    });
+
+    if (!conexao.ok) {
+        throw new Error("Nao foi possivel enviar o video");
+    }
+
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida
+}
+
+async function buscaVideo (termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`)
+    const conexaoConvertida = conexao.json();
+
+    return conexaoConvertida;
+}
+
+export const conectaApi = {
+    listaVideos,
+    criaVideo,
+    buscaVideo
+}
